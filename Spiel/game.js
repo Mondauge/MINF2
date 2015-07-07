cc1724cd559f2fe619aa3aabbab50330f31b9656
Torben <torben.petrow@hotmail.de>
2015-07-08 01:22:09 +0200

    update

--- Spiel/game.js
+++ Spiel/game.js
@@ -6,10 +6,12 @@ function preload() {
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('ground', 'assets/stone.png');
 
-    game.load.spritesheet('hitler', 'assets/hitlerbewegung.png', 185,349);
-    game.load.spritesheet('putin', 'assets/putinbewegung.png', 185,349);
+    game.load.spritesheet('hitler', 'assets/hitlerbewegung2.png', 185, 349);
+    game.load.spritesheet('putin', 'assets/putinbewegung2.png', 185, 349);
     game.load.spritesheet('controller', 'assets/controller-indicator.png', 16,16);
-    game.load.spritesheet('healthbar', 'assets/healthbar.png',300,50 , 4);
+    game.load.spritesheet('healthbar', 'assets/healthbar1.png', 400, 100);
+    game.load.spritesheet('bullet1', 'assets/BULLETrechts.png', 80, 50);
+    game.load.spritesheet('bullet2', 'assets/BULLETlinks.png', 80, 50);
 
 }
 
@@ -24,17 +26,22 @@ var player2;
 var indicator1;
 var indicator2;
 
-var health1 = 100; 
-var health2 = 100; 
+var health1 = 10; 
+var health2 = 10; 
 
-var maxHealth = 200;
-
-var minHealth = 0;
+var maxHealth = 10;
 
 var healthbar1;
+var healthbar2;
 
-var bullets;
+var bullets1;
+var bullets2;
 
+var fireRate1 = 1000;
+var nextFire1 = 1;
+
+var fireRate2 = 1000;
+var nextFire2 = 1;
 
 var music;
 
@@ -52,7 +59,7 @@ function create() {
     //Assign it so we can reference it 
     music = game.add.audio('music');
     music.loop = true;
-    music.volume = 1;
+    music.volume = 0;
     music.play();
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -75,13 +82,24 @@ function create() {
     ground.body.immovable = true;
 
 
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets1 = game.add.group();
+    bullets1.enableBody = true;
+    bullets1.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets1.createMultiple(30, 'bullet1', 0, false);
+    bullets1.setAll('anchor.x', 0.5);
+    bullets1.setAll('anchor.y', 0.5);
+    bullets1.setAll('outOfBoundsKill', true);
+    bullets1.setAll('checkWorldBounds', true);
 
-    bullets.createMultiple(50, 'bullet');
-    bullets.setAll('checkWorldBounds', true);
-    bullets.setAll('outOfBoundsKill', true);
+    bullets2 = game.add.group();
+    bullets2.enableBody = true;
+    bullets2.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets2.createMultiple(30, 'bullet1', 0, false);
+    bullets2.setAll('anchor.x', 0.5);
+    bullets2.setAll('anchor.y', 0.5);
+    bullets2.setAll('outOfBoundsKill', true);
+    bullets2.setAll('checkWorldBounds', true);
+
 
 
     player1 =game.add.sprite(440, game.world.height - 385, 'hitler');
@@ -98,9 +116,11 @@ function create() {
     	indicator2.scale.x = indicator2.scale.y = 2;
     	indicator2.animations.frame = 1;
     	player2.anchor.setTo(0,0);
+        player2.alive = true;
+        player2.block = false;
 
     healthbar1 = this.game.add.sprite(100, 50,'healthbar');
-        healthbar1.animations.add('pain', [2]);
+        //healthbar1.animations.add('pain', [2]);
         healthbar1.cropEnabled = true;
         healthbar1.crop.width = (player1.health1 / player1.maxHealth1) * healthbar1.width;
 
@@ -112,7 +132,9 @@ function create() {
         player1.animations.add('left', [7]);
     	player1.animations.add('right', [6]);
         player1.animations.add('stop', [4]);
-        player1.animations.add('hit', [9]);
+        player1.animations.add('hit', [10]);
+        player1.animations.add('shoot', [8]);
+        player1.animations.add('block', [10]);
  
 
  	game.physics.arcade.enable(player2);
@@ -125,150 +147,168 @@ function create() {
     	player2.animations.add('right', [0]);
         player2.animations.add('stop', [3]);
         player2.animations.add('hit', [8]);
-
-  
+        player2.animations.add('shoot', [8]);
+        player2.animations.add('block', [10]);
 }
+
 function update() {
 
 	game.physics.arcade.collide(player1, platforms);
 	game.physics.arcade.collide(player2, platforms);
+    game.physics.arcade.collide(player1, player2);
+    game.physics.arcade.overlap(bullets2, player1, bulletHitPlayer1, null, this);
+    game.physics.arcade.overlap(bullets1, player2, bulletHitPlayer2, null, this);
 
-    game.physics.arcade.collide(player1, player2,gethit1,null,this);
-
-	    player1.body.velocity.x = 0;
-	    player2.body.velocity.x = 0;
-	    player1.body.drag.y = -150;
-        player2.body.drag.y = -150;
-
-
-  buttonA = pad1.getButton(Phaser.Gamepad.XBOX360_A);
-  buttonB = pad1.getButton(Phaser.Gamepad.XBOX360_B);
-  buttonX = pad1.getButton(Phaser.Gamepad.XBOX360_X);
-
-  //      if (button.buttonCode === Phaser.Gamepad.XBOX360_A)
-  //  {
- //       player1.animations.play('hit');
- //   }
+	player1.body.velocity.x = 0;
+    player2.body.velocity.x = 0;	    
+    player1.body.drag.y = -150;
+    player2.body.drag.y = -150;
+    player1.block = false;
+    player2.block = false;
+    game.lockRender = false;
 
 
-
-    if(game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad1.connected) {
+    if(game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad1.connected && player2.alive) {
         indicator1.animations.frame = 0;
     } else {
         indicator1.animations.frame = 1;
     }
-    if(game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad2.connected) {
+    if(game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad2.connected && player2.alive) {
         indicator2.animations.frame = 0;
     } else {
         indicator2.animations.frame = 1;
     }
 
-        if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
+        if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1 && player2.alive)
     {
          player1.body.velocity.x = -300;
 
         player1.animations.play('left');
     }
-        else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1 && player2.alive)
     {
          player1.body.velocity.x = 300;
 
         player1.animations.play('right');
     }
 
-    	else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP)  || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5 && player1.body.touching.down)
+    	else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP)  || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5 && player1.body.touching.down && player2.alive)
         {
     	player1.body.velocity.y = -250;
         }
 
-        else if (pad1.isDown(Phaser.Gamepad.XBOX360_A)){
-            player1.animations.play('hit');
-        }
-
-
-        else if (pad1.isDown(Phaser.Gamepad.XBOX360_B)){
-            player1.animations.play('hit');
-        }
-
-        else if (pad1.isDown(Phaser.Gamepad.XBOX360_X)){
-            player1.animations.play('hit');
-        }
-
-        else
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_A) && player2.alive){
         
-    {
+            player1.animations.play('block');
+            player1.block = true;
+        }
+
+
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_B) && player2.alive){
+          player1.animations.play('shoot');
+          fire1();
+        }
+
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_X) && player2.alive){
+                player1.animations.play('hit');
+        } else {
+        
         player1.animations.play('stop');
+        
+        }
 
-    }
 
+    if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1 && player1.alive) {
+    player2.body.velocity.x = -300;
+    player2.animations.play('left');
 
-        if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
-    {
-          player2.body.velocity.x = -300;
-
-        player2.animations.play('left');
-    }
-
-        else if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
-    {
-         player2.body.velocity.x = 300;
-
+    } else if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1 && player1.alive) {
+        player2.body.velocity.x = 300;
         player2.animations.play('right');
-    }
-        
-        else if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_UP)  || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5 && player2.body.touching.down)
-        {
+
+    } else if (pad2.isDown(Phaser.Gamepad.XBOX360_DPAD_UP)  || pad2.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5 && player2.body.touching.down && player1.alive) {
         player2.body.velocity.y = -250;
-        }
+    
+    } else if (pad2.isDown(Phaser.Gamepad.XBOX360_A) && player1.alive){
 
-    else if (pad2.isDown(Phaser.Gamepad.XBOX360_A)){
+            player2.animations.play('block');
+            player2.block = true;
 
-            player2.animations.play('hit');
+    } else if (pad2.isDown(Phaser.Gamepad.XBOX360_B) && player1.alive){
 
-        }
+            player2.animations.play('shoot');
+            fire2();
 
-    else if (pad2.isDown(Phaser.Gamepad.XBOX360_B)){
-
-            player2.animations.play('hit');
-
-        }
-
-    else if (pad2.isDown(Phaser.Gamepad.XBOX360_X)){
+    } else if (pad2.isDown(Phaser.Gamepad.XBOX360_X) && player1.alive){
 
             player2.animations.play('hit');
+            game.gamePaused();
 
-        }
-
-        else
-        
-    {
-        player2.animations.play('stop');
-
+    } else {
+            player2.animations.play('stop');
     }
     
-
-
 
     var healthbar2 = this.game.add.sprite(900, 50,'healthbar');
     healthbar2.cropEnabled = true;
     healthbar2.crop.width = (player2.health2 / player2.maxHealth2) * healthbar2.width;
 
-
- //   game.debug.text(health1, 100,300 , "White", "30px Arial");
- //   game.debug.text(health2, 1100,300 , "black", "30px Arial");
-}
-
-
-function gethit1(player1,player2){
-
-    health1 --;
-
-
+    if(health2 <= 0){
+        player2.kill();
+        player1.animations.play('hit');
+    } else if (health1 <= 0) {
+        player1.kill();
+    }
     
-    healthbar1.animations.play('pain');
+    game.debug.text(health1, 100,300 , "White", "30px Arial");
+    game.debug.text(health2, 1100,300 , "black", "30px Arial");
 
 }
 
-function fire (player1){
+function bulletHitPlayer1 (player1, bullet2) {
+
+    bullet2.kill();
+    if(!player1.block) {
+        health1 --;
+    }
+
+}
+
+function bulletHitPlayer2 (player2, bullet1) {
+
+    bullet1.kill();
+    if(!player2.block) {
+        health2 --;
+    }
+
+}
+
+function fire1 () {
+
+    if (game.time.now > nextFire1 && bullets1.countDead() > 0)
+    {
+        nextFire1 = game.time.now + fireRate1;
+
+        var bullet1 = bullets1.getFirstExists(false);
+
+        bullet1.reset(player1.x+180, player1.y+35);
+
+        bullet1.rotation = game.physics.arcade.moveToObject(bullet1, player2, 500);
+    }
+
+}
+
+function fire2 () {
+
+    if (game.time.now > nextFire2 && bullets2.countDead() > 0)
+    {
+        nextFire2 = game.time.now + fireRate2;
+
+        var bullet2 = bullets2.getFirstExists(false);
+
+        bullet2.reset(player2.x+180, player2.y+35);
+
+        bullet2.rotation = game.physics.arcade.moveToObject(bullet2, player1, 500);
+    }
 
 }
